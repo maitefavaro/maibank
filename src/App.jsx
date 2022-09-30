3b0ef591363fddde2b11abd20d0e245130643f58
maitefavaro <maitefavarodacruz@outlook.com>
2022-09-30 11:28:08 -0300

    primeira section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import  React, {useState}  from 'react';
 import { Fullpage,FullPageSections,FullpageSection,FullpageNavigation } from '@ap.cx/react-fullpage';
 import bgimg from '../public/img/back_desfocada.jpg'
 import { MaskField } from 'react-mask-field';
-import { height } from '@mui/system';
+import { css, height } from '@mui/system';
 import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
@@ -22,24 +22,21 @@ export const App = () => {
         <Navbar/>
 
           <FullpageSection style={{height: '100vh'}}>
-            <div className="flex justify-between bg-center bg-cover h-full items-center" 
-              style={{
-                backgroundImage: `url(${bgimg})`,
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat",
-                backgroundSize: "cover",
-              }}>
-              <div className="bg-white w-[30%] h-fit px-16 py-16 rounded-xl flex flex-col gap-2 ml-20 mb-60">
+            <div className="flex justify-around bg-center bg-cover h-full items-center bg-gradient-to-r from-azulclaro to-black" >
+              <div className="bg-white w-[30%] h-fit px-16 py-16 rounded-xl flex flex-col gap-2">
                 <div className=' space-y-9'>
                   <h1 className="text-4xl font-bold ">Peça sua conta e cartão do MaiBank</h1>
                   {/* <MaskedInput value={cpf} onChange={(event) => setCpf(event.target.value)} className="outline-hidden" type="text" /> */}
                   <MaskField className="teste" placeholder="Digite seu CPF" mask="___.___.___-__" replacement={{ _: /\d/ }} />
                 </div>
-                <button className="w-32 p-2 mt-16 ml-auto border-transparent rounded-lg text-white bg-verdeagua hover:bg-verdeagua_escuro">Continuar</button>
+                <button className="w-32 p-2 mt-16 ml-auto border-transparent rounded-lg text-white bg-azulclaro hover:bg-azulescuro">Continuar</button>
             </div>
 
-            <div className="rounded-xl">
-              <img className="h-screen mr-2 opacity-95" src="img/dario2.png" alt="Erro" />
+            <div className="text-white w-[60rem]">
+              <h1 className="negao">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+              <p className='negao2 pt-5'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, esse quos distinctio ducimus quas voluptas aperiam, possimus, iste aspernatur nostrum rerum sint. Esse laudantium quia fugit inventore illo numquam possimus. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et repudiandae ab quam non, quod pariatur sit, maiores perspiciatis iure consequuntur nihil libero temporibus corrupti reiciendis, culpa dolore labore ipsam magni!
+              </p>
             </div>
 
           </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,9 @@ module.exports = {
     extend: {
 
       colors: {
-        bonitinho: "blue",
+        azulescuro: "#143655",
+        azulclaro: "#0740BC",
+        btnlogin: "#0740BC",
         verdeagua: "#04D6C8",
         cinzaa: "#F5F5F5",
         verdeagua_escuro: "#2FB2A9"
